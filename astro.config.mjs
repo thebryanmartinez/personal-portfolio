b4996a8355139cb0b798036edee268267c5fd7ac
thebryanmartinez <bryanmartz972@gmail.com>
2024-03-29 11:57:26 -0600

    feat: 🎸 add partytown and google analytics implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
