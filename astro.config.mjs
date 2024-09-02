14c44c1fc09f267e0738fec83b3e06374b699823
Bryan Martinez <bryanmartz972@gmail.com>
2024-09-02 11:02:11 -0600

    fix: 🐛 Remove image processing from Astro for showing images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     icon(),
     partytown({
